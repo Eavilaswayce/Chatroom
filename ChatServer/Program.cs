c98446838c409bb1791c47a5975394fb4f2538bb
Eavilaswayce <adamrissetto@gmail.com>
2021-05-09 03:59:58 +0100

    Changed Server to Console App

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,46 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ChatServer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    class Program
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            ServerAccess();
-        }
-
-        static void ServerAccess()
+        static void Main(string[] args)
         {
             TcpListener server = null;
             try
             {
                 // Set the TcpListener on port 13000.
-                Int32 port = 13000;
-                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                Int32 port = Int32.Parse("13000");
+                IPAddress localAddr = IPAddress.Parse("192.168.0.11");
 
                 // TcpListener server = new TcpListener(port);
                 server = new TcpListener(localAddr, port);
@@ -54,12 +27,12 @@ namespace ChatServer
 
                 while (true)
                 {
-                    Debug.Write("Waiting for a connection... ");
+                    Console.WriteLine("Waiting for a connection... ");
 
                     // Perform a blocking call to accept requests.
                     // You could also use server.AcceptSocket() here.
                     TcpClient client = server.AcceptTcpClient();
-                    Debug.WriteLine("Connected!");
+                    Console.WriteLine("Connected!");
 
                     data = null;
 
@@ -73,7 +46,7 @@ namespace ChatServer
                     {
                         // Translate data bytes to a ASCII string.
                         data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                        Debug.WriteLine("Received: {0}", data);
+                        Console.WriteLine($"Received: {data}");
 
                         // Process the data sent by the client.
                         data = data.ToUpper();
@@ -82,7 +55,7 @@ namespace ChatServer
 
                         // Send back a response.
                         stream.Write(msg, 0, msg.Length);
-                        Debug.WriteLine("Sent: {0}", data);
+                        Console.WriteLine($"Sent: {data}");
                     }
 
                     // Shutdown and end connection
@@ -91,7 +64,7 @@ namespace ChatServer
             }
             catch (SocketException e)
             {
-                Debug.WriteLine("SocketException: {0}", e);
+                Console.WriteLine($"SocketException: {e}");
             }
             finally
             {
