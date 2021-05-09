b2f0cdcfdf3d83940f7a69e05431cfa03c0bdedf
Aaron Garner <garnermaaron99@gmail.com>
2021-05-08 20:03:47 -0400

    Added Base Foundation
    Added the base foundation for the ChatRoom
    
    Co-Authored-By: Eavilaswayce <45886829+Eavilaswayce@users.noreply.github.com>

--- HellYeah/obj/Debug/net5.0-windows/MainWindow.g.cs
+++ HellYeah/obj/Debug/net5.0-windows/MainWindow.g.cs
@@ -53,7 +53,7 @@ namespace Chatroom {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Chatroom;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Chatroom;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
