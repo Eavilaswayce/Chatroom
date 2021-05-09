1d121240742c8a82f7b17464042cca865b173c54
Eavilaswayce <adamrissetto@gmail.com>
2021-05-09 23:46:17 +0100

    t

--- ChatRoom/MainWindow.xaml.cs
+++ ChatRoom/MainWindow.xaml.cs
@@ -170,8 +170,6 @@ namespace ChatRoom
 
         private void connectButton_Click(object sender, RoutedEventArgs e)
         {
-            Thread thread = new Thread(StartListening);
-            thread.Start();
             Connect();
         }
 
@@ -196,130 +194,6 @@ namespace ChatRoom
                 Send(client, chatBox.Text);
                 chatBox.Text = "";
             }
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        public static ManualResetEvent allDone = new ManualResetEvent(false);
-
-        public void StartListening()
-        {
-            System.Net.WebRequest.DefaultWebProxy.Credentials = System.Net.CredentialCache.DefaultNetworkCredentials;
-
-            // Establish the local endpoint for the socket.  
-            // The DNS name of the computer  
-            // running the listener is "host.contoso.com".  
-            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
-            IPAddress ipv4 = Array.FindLast(Dns.GetHostEntry(Dns.GetHostName()).AddressList, x => x.AddressFamily == AddressFamily.InterNetwork);
-            Debug.WriteLine($"Using IP address: {ipv4}");
-
-            IPEndPoint localEndPoint = new IPEndPoint(ipv4, 53);
-
-            // Create a TCP/IP socket.  
-            Socket listener = new Socket(AddressFamily.InterNetwork,
-                SocketType.Stream, ProtocolType.Tcp);
-
-            // Bind the socket to the local endpoint and listen for incoming connections.  
-            try
-            {
-                listener.Bind(localEndPoint);
-                listener.Listen(100);
-
-                while (true)
-                {
-                    // Set the event to nonsignaled state.  
-                    allDone.Reset();
-
-                    // Start an asynchronous socket to listen for connections.  
-                    Debug.WriteLine("Waiting for a connection...");
-                    listener.BeginAccept(
-                        new AsyncCallback(AcceptCallback),
-                        listener);
-
-                    // Wait until a connection is made before continuing.  
-                    allDone.WaitOne();
-                }
-
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine(e.ToString());
-            }
-
-            Debug.WriteLine("\nPress ENTER to continue...");
-            Console.Read();
-
-        }
-
-        public void AcceptCallback(IAsyncResult ar)
-        {
-            // Signal the main thread to continue.  
-            allDone.Set();
-
-            // Get the socket that handles the client request.  
-            Socket listener = (Socket)ar.AsyncState;
-            Socket handler = listener.EndAccept(ar);
-
-            // Create the state object.  
-            StateObject state = new StateObject();
-            state.workSocket = handler;
-            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
-                new AsyncCallback(ReadCallback), state);
-        }
-
-        public static string message;
-
-        public void ReadCallback(IAsyncResult ar)
-        {
-            String content = String.Empty;
-
-            // Retrieve the state object and the handler socket  
-            // from the asynchronous state object.  
-            StateObject state = (StateObject)ar.AsyncState;
-            Socket handler = state.workSocket;
-
-            // Read data from the client socket.
-            int bytesRead = handler.EndReceive(ar);
-
-            if (bytesRead > 0)
-            {
-                // There  might be more data, so store the data received so far.  
-                state.sb.Append(Encoding.ASCII.GetString(
-                    state.buffer, 0, bytesRead));
-
-                // Check for end-of-file tag. If it is not there, read
-                // more data.  
-                content = state.sb.ToString();
-                // All the data has been read from the
-                // client. Display it on the console.  
-                Debug.WriteLine("Read {0} bytes from socket. \n Data : {1}",
-                    content.Length, content);
-                message = content;
-
-                UpdateMessages();
-            }
-        }
-
-        public void UpdateMessages()
-        {
-            this.Dispatcher.Invoke(() =>
-            {
-            bigBox.AppendText($"{Environment.NewLine}{message}");
-        });
         }
     }
 }
