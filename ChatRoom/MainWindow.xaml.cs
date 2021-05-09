c2e5a880c4a2193ae426138eaa325c25999716cb
Eavilaswayce <adamrissetto@gmail.com>
2021-05-09 05:30:12 +0100

    f

--- ChatRoom/MainWindow.xaml.cs
+++ ChatRoom/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -61,7 +62,7 @@ namespace ChatRoom
                 Int32 bytes = stream.Read(data, 0, data.Length);
                 responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
                 Debug.WriteLine($"Recieved: {responseData}");
-                //bigBox.AppendText($"{username.Text}: {message}{Environment.NewLine}");
+                bigBox.AppendText($"{username.Text}: {message}{Environment.NewLine}");
 
                 // Close Everything
                 stream.Close();
@@ -77,12 +78,88 @@ namespace ChatRoom
             }
         }
 
+        public void Listener()
+        {
+            TcpListener server = null;
+
+            try
+            {
+                // Set the TcpListener on port 13000.
+                Int32 port = Int32.Parse("13000");
+                IPAddress localAddr = IPAddress.Parse("192.168.0.11");
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                while (true)
+                {
+                    Debug.WriteLine("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests.
+                    // You could also use server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    Debug.WriteLine("Connected!");
+
+                    data = null;
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    // Loop to receive all the data sent by the client.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Debug.WriteLine($"Received: {data}");
+
+                        // Process the data sent by the client.
+                        data = data.ToUpper();
+
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                        Debug.WriteLine($"Sent: {data}");
+                        //bigBox.AppendText($"{data}{Environment.NewLine}");
+                        MessageBox.Show("New message: " + data);
+                    }
+
+                    // Shutdown and end connection
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Debug.WriteLine($"SocketException: {e}");
+            }
+            finally
+            {
+                server.Stop();
+            }
+        }
+
         private void chatBox_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
             {
                 Connect(ipTextBox.Text, chatBox.Text);
             }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            new Thread(delegate () {
+                Listener();
+            }).Start();
         }
     }
 }
